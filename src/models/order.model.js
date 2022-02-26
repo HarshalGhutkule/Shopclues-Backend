63172f29b29f90709b988fc0444c78bd2c763831
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-02-27 00:14:54 +0530

    update

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    image: { type: String, required: true },
-    product_name: { type: String, required: true },
+    image: { type: String, required: true,trim: true },
+    product_name: { type: String, required: true,trim: true },
     price: { type: String, required: true },
     user_id: {
       type: mongoose.Schema.Types.ObjectId,
